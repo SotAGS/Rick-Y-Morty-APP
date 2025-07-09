dcb509aeba5c7e429ede5c7c7ba223982b69bdcc
SotAGS <alejandrogalli8@gmail.com>
2025-07-09 18:30:26 -0300

    es5

--- script.js
+++ script.js
@@ -1,63 +1,69 @@
-const results = document.getElementById("results");
-const allBtn = document.getElementById("allBtn");
-const filterBtn = document.getElementById("filterBtn");
+var results = document.getElementById("results");
+var allBtn = document.getElementById("allBtn");
+var filterBtn = document.getElementById("filterBtn");
 
-const API_URL = "https://rickandmortyapi.com/api/character/";
+var API_URL = "https://rickandmortyapi.com/api/character/";
 
 function createCard(character) {
-  return `
-    <div class="card">
-      <img src="${character.image}" alt="${character.name}">
-      <h3>${character.name}</h3>
-      <p>Status: ${character.status}</p>
-      <p>Species: ${character.species}</p>
-      <p>Gender: ${character.gender}</p>
-    </div>
-  `;
+  return '<div class="card">' +
+    '<img src="' + character.image + '" alt="' + character.name + '">' +
+    '<h3>' + character.name + '</h3>' +
+    '<p>Status: ' + character.status + '</p>' +
+    '<p>Species: ' + character.species + '</p>' +
+    '<p>Gender: ' + character.gender + '</p>' +
+  '</div>';
 }
 
 function renderCharacters(characters) {
-  results.innerHTML = characters.map(createCard).join("");
-}
-
-function showError(error) {
-  results.innerHTML = `<p style="color:red;">Error: ${error}</p>`;
-}
-
-async function fetchCharacters(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error("Request fallida");
-    const data = await response.json();
-    renderCharacters(data.results);
-  } catch (error) {
-    showError(error.message);
+  var html = "";
+  for (var i = 0; i < characters.length; i++) {
+    html += createCard(characters[i]);
   }
+  results.innerHTML = html;
+}
+
+function showError(message) {
+  results.innerHTML = '<p style="color:red;">Error: ' + message + '</p>';
+}
+
+function fetchCharacters(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        renderCharacters(response.results);
+      } else {
+        showError("No se pudo obtener la información");
+      }
+    }
+  };
+  xhr.send();
 }
 
 function getFilterURL() {
-  const name = document.getElementById("name").value;
-  const status = document.getElementById("status").value;
-  const species = document.getElementById("species").value;
-  const type = document.getElementById("type").value;
-  const gender = document.getElementById("gender").value;
+  var name = document.getElementById("name").value;
+  var status = document.getElementById("status").value;
+  var species = document.getElementById("species").value;
+  var type = document.getElementById("type").value;
+  var gender = document.getElementById("gender").value;
 
-  const params = new URLSearchParams();
+  var params = [];
+  if (name) params.push("name=" + encodeURIComponent(name));
+  if (status) params.push("status=" + encodeURIComponent(status));
+  if (species) params.push("species=" + encodeURIComponent(species));
+  if (type) params.push("type=" + encodeURIComponent(type));
+  if (gender) params.push("gender=" + encodeURIComponent(gender));
 
-  if (name) params.append("name", name);
-  if (status) params.append("status", status);
-  if (species) params.append("species", species);
-  if (type) params.append("type", type);
-  if (gender) params.append("gender", gender);
-
-  return `${API_URL}?${params.toString()}`;
+  return API_URL + "?" + params.join("&");
 }
 
-allBtn.addEventListener("click", () => {
+allBtn.addEventListener("click", function () {
   fetchCharacters(API_URL);
 });
 
-filterBtn.addEventListener("click", () => {
-  const url = getFilterURL();
+filterBtn.addEventListener("click", function () {
+  var url = getFilterURL();
   fetchCharacters(url);
 });
